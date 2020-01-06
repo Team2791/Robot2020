c952519bb68050ece7293fce9250d122364000cf
fidgetspinnerkid <35818373+fidgetspinnerkid@users.noreply.github.com>
2020-01-06 17:31:01 -0500

    Update Drivetrain.java

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -107,16 +107,6 @@ public class Drivetrain extends Subsystem {
         }
     }
 
-    // private void setRamp(double rampRate)
-    // {
-    //     leftLeader.setOpenLoopRampRate(rampRate);
-    //     rightLeader.setOpenLoopRampRate(rampRate);
-    //     for(int i = 0; i < leftFollowers.length; ++i) {
-    //         leftFollowers[i].setOpenLoopRampRate(rampRate);
-    //         rightFollowers[i].setOpenLoopRampRate(rampRate);
-    //     }
-    // }
-
     public void initDefaultCommand() {
         if(defaultCommand == null) {
             defaultCommand = new StopDrive();
@@ -127,17 +117,8 @@ public class Drivetrain extends Subsystem {
     public void setMotors(double left, double right) {
         SmartDashboard.putNumber("LeftSideOutput", left);
         SmartDashboard.putNumber("RightSideOutput", right);
-
-        if(getLeftVelocity() < 0 && getRightVelocity() < 0 && (left + right) / 2 > 0) { //TODO change the first 0 and second 0 to some type of threshold value
-            // leftLeader.set(Math.max(0, left * speedMultiplier));
-            // rightLeader.set(Math.max(0, right * speedMultiplier));
-            leftLeader.set(0);
-            rightLeader.set(0);
-            System.out.println("Wheelie prevention");
-        } else {
-            leftLeader.set(left * speedMultiplier);
-            rightLeader.set(right * speedMultiplier);
-        }
+        leftLeader.set(left * speedMultiplier);
+        rightLeader.set(right * speedMultiplier);
     }
 
     public void setBrakeMode(boolean isbrake) {
@@ -158,7 +139,14 @@ public class Drivetrain extends Subsystem {
         return (int)(rightLeader.getEncoder().getPosition() + 0.5); //+1/2 for rounding
     }
 
+
+    /*
+    This updates the velocity of the robot.
+    Records the last 5 recorded velocities.
+    Utilized in getVelocity() which eventually gets printed on SmartDashboard.
+    */
     public void update() {
+
         int leftEnc = getLeftEncoder();
         int rightEnc = getRightEncoder();
 
@@ -212,60 +200,6 @@ public class Drivetrain extends Subsystem {
         rightVelocities[frameCounter % 5] = right;
     }
 
-    // public double getGyroAngle(){
-    //     if(!gyroDisabled)
-    //         return gyro.getAngle();
-    //     System.err.println("Gyro is Disabled, Angle is Incorrect");
-    //     return 0.0;
-    // }
-    // public double getGyroRate() {
-    //     if (!gyroDisabled)
-    //         return gyro.getRate();
-    //     System.err.println("Gyro is Disabled, Rate is Incorrect");
-    //     return 0.0;
-    // }
-    // public double getGyroAngleInRadians() {
-    //     return getGyroAngle() * (Math.PI/180);
-    // }
-    // public void resetGyro() {
-    //     if(!gyroDisabled) {
-    //         gyro.reset();
-    //     } else { 
-    //         System.err.println("Gyro is Disabled, Unable to Reset");
-    //     }
-    // }
-    // public boolean getGyroDisabled() {
-    //     return gyroDisabled;
-    // } 
-    // public void calibrateGyro() {
-    //     if (!gyroDisabled) {
-    //         System.out.println("Gyro calibrating");
-    //         gyro.calibrate();
-    //         System.out.println("Done calibrating " + " The current rate is " + gyro.getRate());
-    //     }
-    // }
-
-    public int getLineSensors() {
-        int res = 0;
-        res |= lineSensors[0].getAverageVoltage() > Constants.kLineVoltCutoff ? 1 : 0;
-        res |= lineSensors[1].getAverageVoltage() > Constants.kLineVoltCutoff ? 2 : 0;
-        res |= lineSensors[2].getAverageVoltage() > Constants.kLineVoltCutoff ? 4 : 0;
-        res |= lineSensors[3].getAverageVoltage() > Constants.kLineVoltCutoff ? 8 : 0;
-        lineFound = (res & 15) > 0;
-        setBlueLED(lineFound);
-        return res;
-    }
-    public void setGreenLED(boolean state){
-        greenLED.set(state);
-    }
-    public void setBlueLED(boolean state){
-        blueLED.set(state);
-    }
-    public double getLineSensor(int index)
-    {
-        return lineSensors[index].getAverageVoltage();
-    }
-
     public void debug(){
         SmartDashboard.putNumber("Left Encoder", getLeftEncoder());
         SmartDashboard.putNumber("Right Encoder", getLeftEncoder());
@@ -274,10 +208,5 @@ public class Drivetrain extends Subsystem {
         SmartDashboard.putNumber("Left Drivetrain 1", Robot.pdp.getCurrent(14));
         SmartDashboard.putNumber("Left Drivetrain 2", Robot.pdp.getCurrent(15));
         SmartDashboard.putNumber("Measured Velocity", getVelocity());
-        SmartDashboard.putNumber("Line Active", getLineSensors());
-        SmartDashboard.putBoolean("Line Found", lineFound);
-        for(int i = 0; i < 4; ++ i) {
-            SmartDashboard.putNumber("Line Voltage" + i, getLineSensor(i));
-        }
     }
 }  
