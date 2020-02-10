023c087e6cf84b8f9eca16b033017778994ca630
Tim Oh - Team2791 <timbim1681@gmail.com>
2020-02-10 17:23:53 -0500

    Added Automatic Shooter
    added limelight in util, changed constants, and added a new command

--- src/main/java/frc/robot/commands/AutomaticShooting.java
+++ src/main/java/frc/robot/commands/AutomaticShooting.java
@@ -1,10 +1,3 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;5
@@ -12,9 +5,9 @@ import frc.robot.Constants;
 import frc.robot.Robot;
 import frc.robot.util.Limelight;
 
-public class MoveShooter extends Command {
-  public MoveShooter() {
-    super("MoveShooter");
+public class AutomaticShooting extends Command {
+  public AutomaticShooting() {
+    super("AutomaticShooting");
     requires(Robot.shooter);
 
     // Use requires() here to declare subsystem dependencies
@@ -31,7 +24,7 @@ public class MoveShooter extends Command {
   @Override
   protected void execute() {
     
-    Robot.shooter.setShooter(Constants.SHOOTER_OUTPUT);
+    Robot.shooter.setShooter(idealVelocity(2.0, Constants.WALL_SHOT, Constants.HEIGHT));
   }
 
   // Make this return true when this Command no longer needs to run execute()
