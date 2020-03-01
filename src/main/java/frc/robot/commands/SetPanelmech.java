b7bef51752151322db8ca03464d199f303c691e8
allison-kunin <allison.kunin@gmail.com>
2020-03-01 17:57:20 -0500

    changed setPanelMech so it works hopefully

--- src/main/java/frc/robot/commands/SetPanelmech.java
+++ src/main/java/frc/robot/commands/SetPanelmech.java
@@ -1,54 +1,47 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
+package frc.robot.commands;
 
-// package frc.robot.commands;
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Constants;
+import frc.robot.Robot;
 
-// import edu.wpi.first.wpilibj.command.Command;
-// import frc.robot.Constants;
-// import frc.robot.Robot;
+public class SetPanelmech extends Command {
+    public SetPanelmech() {
+    super("SetPanelmech");
+    requires(Robot.panelMech);
+        //Use requires() here to declare subsystem dependencies
+        //eg. requires(chassis);
+    }
 
-// public class SetPanelMech extends Command {
-//     public SetPanelMech() {
-//     super("SetPanelMech");
-//     requires(Robot.panelMech);
-//         //Use requires() here to declare subsystem dependencies
-//         //eg. requires(chassis);
-//     }
-
-//     //Called just before this Command runs the first time
-//     @Override
-//     protected void initialize() {
+    //Called just before this Command runs the first time
+    @Override
+    protected void initialize() {
+    }
     
-    // 
+    
 
     //Called repeatedly when this Command is scheduled to run 
-    // @Override
-    // protected void execute() {
-    //     Robot.panelMech.setPanelMech(0);
-    // //    Robot.panelMech.setManipulator(0);
-    // }
+    @Override
+    protected void execute() {
+        Robot.panelMech.setPanelMech(Constants.PANEL_MECH_CREEP);
+    }
 
-    // //Make this return true when this Command no longer needs to run execute()
-    // @Override
-    // protected boolean isFinished() {
-    //     return false;
-    // }
+    //Make this return true when this Command no longer needs to run execute()
+    @Override
+    protected boolean isFinished() {
+        return false;
+    }
 
     //Called once after isFinished returns true
-//     @Override
-//     protected void end() {
-//         Robot.panelMech.setPanelMech(0);
-//     }
+    @Override
+    protected void end() {
+        Robot.panelMech.setPanelMech(0);
+    }
 
-// //     //Called when another command which requires one or more of the same 
-// //     //subsystems is scheduled to run
+//     //Called when another command which requires one or more of the same 
+//     //subsystems is scheduled to run
 
-//     @Override
-//     protected void interrupted() {
-//         //Yes
-//     }
-//}
+    @Override
+    protected void interrupted() {
+        //Yes
+    }
+}
