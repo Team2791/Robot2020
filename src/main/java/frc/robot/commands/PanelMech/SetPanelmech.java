0e738cbcdfb54f33465a6169d73900b174a288d5
frcteam2791 <team2791@gmail.com>
2020-03-02 18:23:44 -0500

    changed setpanelmech() to a toggle kind of

--- src/main/java/frc/robot/commands/PanelMech/SetPanelmech.java
+++ src/main/java/frc/robot/commands/PanelMech/SetPanelmech.java
@@ -5,16 +5,21 @@ import frc.robot.Constants;
 import frc.robot.Robot;
 
 public class SetPanelmech extends Command {
+    double count = 1;
     public SetPanelmech() {
-    super("SetPanelmech");
-    requires(Robot.panelMech);
-        //Use requires() here to declare subsystem dependencies
-        //eg. requires(chassis);
+        super("SetPanelmech");
+        requires(Robot.panelMech);
+        
     }
 
     //Called just before this Command runs the first time
     @Override
     protected void initialize() {
+        count++;
+        if(count > 2){
+            count = 0;
+        }
+        Robot.panelMech.extendPanelMech();
     }
     
     
@@ -22,26 +27,38 @@ public class SetPanelmech extends Command {
     //Called repeatedly when this Command is scheduled to run 
     @Override
     protected void execute() {
-        Robot.panelMech.setPanelMech(Constants.PANEL_MECH_CREEP);
+        if(count == 1){
+            Robot.panelMech.setPanelMech(Constants.PANEL_MECH_FAST);
+        }
+        else if(count == 2){
+            Robot.panelMech.setPanelMech(Constants.PANEL_MECH_CREEP);
+        }
+        else{
+            Robot.panelMech.setPanelMech(0);
+        }
     }
 
     //Make this return true when this Command no longer needs to run execute()
     @Override
     protected boolean isFinished() {
-        return true;
+        if(count == 0){
+            return true;
+        }
+        return false;
     }
 
     //Called once after isFinished returns true
     @Override
     protected void end() {
         Robot.panelMech.setPanelMech(0);
+        Robot.panelMech.retractPanelMech();
     }
 
-//     //Called when another command which requires one or more of the same 
-//     //subsystems is scheduled to run
+    //Called when another command which requires one or more of the same 
+    //subsystems is scheduled to run
 
     @Override
     protected void interrupted() {
-        //Yes
+        
     }
 }
